52f1b778594d18336c175b37e41078ffe27dc8c5
ielbadisy <elbadisyimad@gmail.com>
2026-06-17 11:35:32 +0100

    Align outlier workflow API

--- tests/testthat/test-assumptions-structure.R
+++ tests/testthat/test-assumptions-structure.R
@@ -1,10 +1,11 @@
 test_that("workflows return non-empty assumptions and print Assumptions", {
   dat <- make_assumption_data(80)
+  dat$grp3 <- factor(dplyr::ntile(dat$age, 3))
   objs <- list(
     test_one_sample(dat, sbp_3m, mu = 140, plot = FALSE),
     test_two_groups(dat, sbp_3m, sex, plot = FALSE),
-    test_paired(dat, sbp_3m ~ sbp_baseline, data = dat, plot = FALSE),
-    test_groups(dat, sbp_3m, treatment, plot = FALSE),
+    test_paired(sbp_3m ~ sbp_baseline, data = dat, plot = FALSE),
+    test_groups(dat, sbp_3m, grp3, plot = FALSE),
     test_factorial(dat, sbp_3m ~ sex * treatment, plot = FALSE),
     test_repeated(dat, c(sbp_baseline, sbp_3m, sbp_6m), id = id, plot = FALSE),
     test_categorical(dat, treatment, controlled_3m, plot = FALSE),
@@ -14,7 +15,7 @@ test_that("workflows return non-empty assumptions and print Assumptions", {
     test_multinomial(dat, treatment, plot = FALSE),
     test_correlation(sbp_3m ~ age, data = dat, plot = FALSE),
     test_correlation_matrix(dat, c(age, sbp_3m, ldl), plot = FALSE),
-    test_outliers(dat, c(sbp_3m, ldl, crp), plot = FALSE)
+    test_outliers(c(sbp_3m, ldl, crp), data = dat, plot = FALSE)
   )
 
   for (x in objs) {
