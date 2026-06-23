a44dfd80ae172eecb63be28915bb8cf9d6a0927f
ielbadisy <elbadisyimad@gmail.com>
2026-06-23 02:01:42 +0100

    Polish final workflow output labels

--- tests/testthat/test-assumptions-outliers.R
+++ tests/testthat/test-assumptions-outliers.R
@@ -1,7 +1,13 @@
 test_that("outlier methods report different assumptions", {
   dat <- make_assumption_data(60)
-  iqr <- test_outliers(c(sbp_3m), data = dat, method = "iqr", plot = FALSE)
-  mahal <- test_outliers(c(sbp_3m, ldl, crp), data = dat, method = "mahalanobis", plot = FALSE)
+  expect_warning(
+    iqr <- test_outliers(c(sbp_3m), data = dat, method = "iqr", plot = FALSE),
+    "screening workflow"
+  )
+  expect_warning(
+    mahal <- test_outliers(c(sbp_3m, ldl, crp), data = dat, method = "mahalanobis", plot = FALSE),
+    "screening workflow"
+  )
   expect_true(any(grepl("Skewness sensitivity", iqr$assumptions$name)))
   expect_true(any(grepl("Approximate multivariate normality", mahal$assumptions$name)))
 })
